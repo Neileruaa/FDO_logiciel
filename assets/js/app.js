3aa59abfd99205f9d71c8d8c1ef7a93381970b1b
Aurélien <aureliendrey@gmail.com>
2019-01-14 20:35:15 +0100

    Navbar + Materialize font

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,8 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
+require('../css/Montserrat.css');
+
 require('../css/app.scss');
 
 const $ = require('jquery');
