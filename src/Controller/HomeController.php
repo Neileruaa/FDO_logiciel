97cdb0617411afd86102164d1f381a62a3cb3b35
Aurélien <aureliendrey@gmail.com>
2019-01-14 16:41:02 +0100

    Setup Encore with Bootstrap

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="Home.index")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+        ]);
+    }
+}
