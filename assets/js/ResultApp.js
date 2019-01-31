a7a655fe3dcc067ee768035b9a7bffee63472cd9
aurelien <aureliendrey@gmail.com>
2019-01-31 21:37:14 +0100

    Cell table react !

--- assets/js/ResultApp.js
+++ assets/js/ResultApp.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HotTable} from "@handsontable/react";
 
-import TodoTab from './Components/TodoTab';
-import PlanningTab from './Components/PlanningTab'
-import {arrayMove} from "react-sortable-hoc";
-import axios from 'axios';
 
 class ResultApp extends React.Component{
     constructor(props){
         super(props);
-
+        this.data = [
+            ["", "Ford", "Volvo", "Toyota", "Honda"],
+            ["2016", 10, 11, 12, 13],
+            ["2017", 20, 11, 14, 13],
+            ["2018", 30, 15, 12, 13]
+        ];
     }
     render() {
         return(
-            <div>
-                <h1>Test</h1>
+            <div className="hot-app">
+                <h1>Affichage des résultats</h1>
+                <HotTable data={this.data} colHeaders={true} rowHeaders={true} width="auto" height="600" stretchH="all" />
             </div>
         );
     }
