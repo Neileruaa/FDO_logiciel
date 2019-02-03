246065b000133f3f2b9fff7d81b8aadd97d03ed9
aurelien <aureliendrey@gmail.com>
2019-02-03 13:30:32 +0100

    Clean resultat App

--- assets/js/ResultApp.js
+++ assets/js/ResultApp.js
@@ -11,13 +11,6 @@ class ResultApp extends React.Component{
         this.state={
             testData: [],
         };
-
-        this.data = [
-            ["", "Ford", "Volvo", "Toyota", "Honda"],
-            ["2016", 10, 11, 12, 13],
-            ["2017", 20, 11, 14, 13],
-            ["2018", 30, 15, 12, 13]
-        ];
     }
 
     componentDidMount() {
@@ -39,7 +32,7 @@ class ResultApp extends React.Component{
         return(
             <div className="hot-app">
                 <h1>Affichage des résultats</h1>
-                <HotTable data={this.state.testData} minSpareRows={1} />
+                <HotTable data={this.state.testData} colHeaders={true} rowHeaders={true} stretchH="all" width="auto"/>
             </div>
         );
     }
