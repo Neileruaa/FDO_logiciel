3f9202dafc6830c9d4688450fca3c232581a979b
Antoine <antoine.heitzmann2@gmail.com>
2019-02-11 12:48:19 +0100

    création du planning en PDF

--- src/Controller/PanelController.php
+++ src/Controller/PanelController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Competition;
+use App\Entity\Row;
 use Dompdf\Dompdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PanelController extends AbstractController
@@ -20,9 +23,10 @@ class PanelController extends AbstractController
 	/**
 	 * @Route("/testpdf", name="Panel.testPdf")
 	 */
-	public function firstPdfDomPDF() {
+	public function firstPdfDomPDF(SessionInterface $session) {
+	    $compet=$this->getDoctrine()->getRepository(Competition::class)->find($session->get('competSelected'));
 		$dompdf = new Dompdf();
-		$html = $this->renderView('pdf/test.html.twig',[]);
+        $html = $this->renderView('pdf/test.html.twig',['rows'=>$this->getDoctrine()->getRepository(Row::class)->findBy(['competition'=>$compet])]);
 		$dompdf->loadHtml($html);
 		$dompdf->setPaper('A4', 'portrait');
 		$dompdf->render();
