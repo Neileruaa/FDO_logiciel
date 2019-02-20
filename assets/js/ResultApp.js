d6810a5bc98202419e81ba89a76bdd12c2b2c4c9
Neileruaa <aureliendrey@gmail.com>
2019-02-20 11:34:31 +0100

    Ajout des options nb danseurs qualifiés et type tour suivant

--- assets/js/ResultApp.js
+++ assets/js/ResultApp.js
@@ -12,11 +12,15 @@ class ResultApp extends React.Component{
 
         this.state= {
             sheet: [],
-            notes: {}
+            notes: {},
+            nbQualifie: 2,
+            nextRound:""
         };
 
         this.hotTableComponent = React.createRef();
         this.doCalc = this.doCalc.bind(this);
+        this.handleRangeNbQualifie = this.handleRangeNbQualifie.bind(this);
+        this.handleSelectNextRound= this.handleSelectNextRound.bind(this);
     }
 
     componentDidMount() {
@@ -97,7 +101,22 @@ class ResultApp extends React.Component{
         }, {});
     }
 
+    handleRangeNbQualifie(e){
+        this.setState({nbQualifie: e.target.value});
+    }
+
+    handleSelectNextRound(e){
+        this.setState({nextRound:e.target.value})
+    }
+
     render() {
+
+        var tour = "";
+        if (this.state.nextRound === ""){
+            tour = document.getElementById("resultatApp").getAttribute("data-row-numTour");
+        }else
+            tour = this.state.nextRound;
+
         return(
             <div className="hot-app">
                 <h1>Affichage des résultats</h1>
@@ -119,7 +138,24 @@ class ResultApp extends React.Component{
                     }
                 />
                 <button onClick={this.doCalc} className="btn btn-outline-primary">Calculer les moyennes</button>
-
+                <br/>
+                <div>
+                    <label htmlFor="chooseNbQualifie">Choisir nombres de personnes qualifiés : {this.state.nbQualifie}</label>
+                    <input id="chooseNbQualifie" type="range" value={this.state.nbQualifie} className="custom-range" onChange={this.handleRangeNbQualifie} min="2"/>
+                </div>
+                <div>
+                    <label htmlFor="choisirPiste">Choisir un type de tour:</label>
+                    <select name="piste" id="choisirPiste" value={tour} onChange={this.handleSelectNextRound}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3" >3</option>
+                        <option value="4" >4</option>
+                        <option value="Huitieme" >Huitième</option>
+                        <option value="Quart" >Quart de finale</option>
+                        <option value="Demi" >Demi finale</option>
+                        <option value="Finale" >Finale</option>
+                    </select>
+                </div>
                 <ShowResult
                     notes={this.state.notes}
                 />
