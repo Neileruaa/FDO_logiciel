83539fae1169f1bccdbad74d0d5982f1fa25e40c
Neileruaa <aureliendrey@gmail.com>
2019-03-07 11:45:34 +0100

    Style pour ordered list and Finale en rouge

--- assets/js/Components/ShowResult.js
+++ assets/js/Components/ShowResult.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import SortableComponent from "./PlanningTab";
-
+import '../../css/ShowResult.css'
 
 function ShowResult(props){
     const notes = props.notes;
     const listNotes = Object.keys(notes).map((note, index, tab)=>
-        <li key={index}>Equipe {note} = {notes[note]}</li>
+        <li key={index}>Equipe {note} = <span>{notes[note]}</span></li>
     );
     return(
-        <ul>{listNotes}</ul>
+        <ol>{listNotes}</ol>
     )
 }
 
